ba704d1f8816e087922e9708641ffa4d66126db3
Bunny-P-Adams <97325724+Bunny-P-Adams@users.noreply.github.com>
2022-03-07 00:58:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import turtle as t
-t.speed("fastest")
-#t.tracer(0,0)
-# chess piece images
+
+board = [["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
+         ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
+         ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
+         ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
+         ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
+         ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
+         ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
+         ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"]]
+
 chess_pieces = ["white_pawn.gif", "black_pawn.gif", "white_bishop.gif", "black_bishop.gif", "white_knight.gif", "black_knight.gif",
                 "white_rook.gif", "black_rook.gif", "white_queen.gif", "black_queen.gif", "white_king.gif", "black_king.gif"]
 
-# screen init
+piece_setup = ["apawn", "bpawn", "cpawn", "dpawn", "epawn", "fpawn", "gpawn", "hpawn",
+               "arook", "bknight", "cbishop", "dqueen", "eking", "fbishop", "gknight", "hrook"]
+
+white_pieces = []
+black_pieces = []
+
 wn = t.Screen()
 wn.bgcolor("black")
 for piece in chess_pieces:
     wn.addshape(piece)
-
-s = t.Turtle()
-s.shape("square")
-s.color("burlywood")
-s.penup()
-s.turtlesize(5)
-s.goto(-350, -350)
-s.speed(0)
 
 
 class Piece:
@@ -39,70 +43,31 @@ class Piece:
         super().__init__(turtle, shape, team, posX, posY)
 '''
 
-#board = [["" for i in range(8)] for i in range(8)]
-board = [["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
-         ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
-         ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
-         ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
-         ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
-         ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
-         ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
-         ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"]]
-
+xcor = -350
+ycor = -350
+row = 0
+col = 0
 for r in board:
     for c in r:
-        print(c)
         c = t.Turtle()
         c.shape("square")
-'''
-x = 0
-y = 0
-num = 8
-letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
-for i in range(64):
-    #TODO: array of turtles for chess board
-    #name = letters[y] + str(num)
-    
-    name = board[x][y]
-    name = t.Turtle()
-    board[x][y] = t.Turtle()
-    print(board[x][y])
-    y += 1
-    i += 1
-    if i % 2 == 0:
-        board[x][y].color("burlywood")
-    else:
-        board[x][y].color("brown")
-    if i % 8 == 0:
-        x += 1
-        y = 0
-        num -= 1
-'''
-x = 100
-
-for i in range(1, 64):
-    s.stamp()
-
-    s.goto(s.xcor()+x, s.ycor())
-    
-    if i % 2 == 0:
-        s.color("burlywood")
-    else:
-        s.color("brown")
-    if i % 8 == 0:
-        if i % 16 == 0:
-            s.goto(s.xcor()+100, s.ycor()+100)
+        c.turtlesize(5)
+        c.penup()
+        c.speed(0)
+        c.goto(xcor, ycor)
+        xcor += 100
+        
+        if col % 2 == 0:
+            c.color("burlywood")
         else:
-            s.goto(s.xcor()-100, s.ycor()+100)
-        x *= -1
+            c.color("brown")
+       
+        col += 1
+    ycor += 100
+    xcor = -350
+    col += 1
+    row += 1
 
-    
-
-piece_setup = ["apawn", "bpawn", "cpawn", "dpawn", "epawn", "fpawn", "gpawn", "hpawn",
-               "arook", "bknight", "cbishop", "dqueen", "eking", "fbishop", "gknight", "hrook"]
-
-white_pieces = []
-black_pieces = []
 
 x = -350
 y = -250
@@ -124,9 +89,16 @@ for piece in piece_setup:
     black_pieces.append(piece)
     x += 100
 
-for i in white_pieces:
-    print(i.turtle)
+gameActive = True
+while gameActive:
+    turn = 0
+    if turn % 2 == 0:
+        for piece in white_pieces:
+            piece.turtle.ondrag(piece.turtle.goto)
+    else:
+        for piece in black_pieces:
+            piece.turtle.ondrag(piece.turtle.goto)
 
-#t.update()
+    turn += 1
+
 wn.mainloop()
-
